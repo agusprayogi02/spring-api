f23cc642d54236437c5798ef99d13acbfb2345f6
Agus Prayogi <agus21apy@gmail.com>
2022-05-28 13:12:05 +0700

    :hammer: module 5

--- product-api/src/main/java/com/smk/modul1/product/dto/input/ProductInput.java
+++ product-api/src/main/java/com/smk/modul1/product/dto/input/ProductInput.java
@@ -2,12 +2,22 @@ package com.smk.modul1.product.dto.input;
 
 import lombok.*;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ProductInput {
+    //regex only alphabet and space
+    @Pattern(regexp = "[^A-Za-z0-9 ]+$", message = "Only alphabet and space")
+    @NotBlank(message = "Product name cannot be empty")
     private String name;
+
+    @NotBlank(message = "Product description cannot be empty")
     private String description;
-    private Double price;
+
+    @NotBlank(message = "Product price cannot be empty")
+    private String price;
 }
--- product-imp/src/main/java/com/smk/modul1/product/controller/ProductController.java
+++ product-imp/src/main/java/com/smk/modul1/product/controller/ProductController.java
@@ -1,43 +1,57 @@
 package com.smk.modul1.product.controller;
 
+import com.smk.modul1.controller.BaseController;
+import com.smk.modul1.dto.BaseResponse;
 import com.smk.modul1.product.dto.input.ProductInput;
 import com.smk.modul1.product.dto.output.ProductOutput;
+import com.smk.modul1.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
 @RequestMapping("/products")
-public class ProductController {
-    private ArrayList<ProductOutput> listArray;
+public class ProductController extends BaseController {
+    @Autowired
+    private ProductService service;
 
     @GetMapping("/{id}")
-    public ResponseEntity<ProductOutput> getOne(@PathVariable Long id) {
-        return ResponseEntity.ok(ProductOutput.builder().build());
+    public ResponseEntity<BaseResponse<ProductOutput>> getOne(@PathVariable Long id) {
+        ProductOutput product = service.getProduct(id);
+        return ResponseEntity.ok(new BaseResponse<>(product));
     }
 
     @GetMapping
-    public ResponseEntity<List<ProductOutput>> getAllProduct() {
-        ArrayList<ProductOutput> listArray = new ArrayList<>();
-        listArray.add(new ProductOutput(1L, "A", "AA", 10.0));
-        listArray.add(new ProductOutput(2L, "B", "BB", 15.0));
-        return ResponseEntity.ok(listArray);
+    public ResponseEntity<BaseResponse<List<ProductOutput>>> getAllProduct() {
+        List<ProductOutput> listArray = service.getAllProduct();
+        return ResponseEntity.ok(new BaseResponse<>(listArray));
     }
 
     @PostMapping
-    public ResponseEntity<ProductOutput> create(@RequestBody ProductInput productInput) {
-        return ResponseEntity.ok(ProductOutput.builder().build());
+    public ResponseEntity<BaseResponse<ProductOutput>> addOne(@Valid @RequestBody ProductInput productInput) {
+        ProductOutput product = service.addProduct(productInput);
+        return ResponseEntity.ok(new BaseResponse<>(product));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<ProductOutput> update(@PathVariable Long id, @RequestBody ProductInput productInput) {
-        return ResponseEntity.ok(ProductOutput.builder().build());
+    public ResponseEntity<BaseResponse<ProductOutput>> update(@PathVariable Long id,
+                                                @Valid @RequestBody ProductInput productInput) {
+        ProductOutput product = service.updateProduct(productInput, id);
+        return ResponseEntity.ok(new BaseResponse<>(product));
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        return ResponseEntity.ok().build();
+    public ResponseEntity<BaseResponse<Boolean>> delete(@PathVariable Long id) {
+        boolean delete = service.deleteProduct(id);
+        return ResponseEntity.ok(new BaseResponse<>(delete));
+    }
+
+    @PostMapping("/saveAll")
+    public ResponseEntity<BaseResponse<List<ProductOutput>>> saveAllProduct(@Valid @RequestBody Iterable<ProductInput> inputs){
+        List<ProductOutput> rests = service.multipleProduct(inputs);
+        return ResponseEntity.ok(new BaseResponse<>(rests));
     }
 }
